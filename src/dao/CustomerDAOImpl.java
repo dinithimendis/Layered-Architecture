33170e4841d47332050897e82310adebfab43bac
dinithimendis <dinithimendis42@gmail.com>
2023-01-08 13:09:27 -0800

    initial commit

---
+++ src/dao/CustomerDAOImpl.java
@@ -0,0 +1,28 @@
+package dao;
+
+import db.DBConnection;
+import model.CustomerDTO;
+import view.tdm.CustomerTM;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class CustomerDAOImpl {
+
+    public ArrayList<CustomerDTO> getAllCustomers() throws SQLException, ClassNotFoundException {
+        ArrayList<CustomerDTO> allCustomers= new ArrayList<>();
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        Statement stm = connection.createStatement();
+        ResultSet rst = stm.executeQuery("SELECT * FROM Customer");
+
+        while (rst.next()) {
+            CustomerDTO customerDTO = new CustomerDTO(rst.getString("id"), rst.getString("name"), rst.getString("address"));
+            allCustomers.add(customerDTO);
+        }
+        return allCustomers;
+    }
+
+}
